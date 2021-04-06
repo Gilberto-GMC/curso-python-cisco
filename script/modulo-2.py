2d9a295c9dda0e5f28d2849a5c97d69c19240bd3
Gilberto Marques Claudino <gilberto.claudino@fatec.sp.gov.br>
2021-04-06 10:50:32 -0300

    Arquivo referente ao modulo 2

---
+++ script/modulo-2.py
@@ -0,0 +1,24 @@
+print("My name is", "Python.")
+print("Monty Python.")
+
+print("My name is", "Python.", end=" ")
+print("Monty Python.")
+
+print("My", "name", "is", "Monty", "Python.", sep="-")
+
+print("My", "name", "is", sep="_", end="*")
+print("Monty", "Python.", sep="*", end="*\n")
+
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+#Os parâmetros END e SEP podem ser usados ​​para formatar a saída da print()função.
+#O SEP parâmetro especifica o separador entre os argumentos de saída por exemplo,
+print("H", "E", "L", "L", "O", sep="-")
+#enquanto o END parâmetro especifica o que imprimir no final da instrução de impressão.
